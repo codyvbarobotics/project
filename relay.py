c5e8a20b23f267566b6c9117264a75afa414ea29
cody stephens <cstephens@vbainfo.com>
2019-02-22 20:24:08 +0000

    added file

--- relay.py
+++ relay.py
@@ -1,8 +1,8 @@
-import RPi.GPIO,time
+import RPi.GPIO as GPIO,time
 
-GPIO.setmode(GPIO.BMC)
-GPIO.setwarnings(FALSE)
-red= 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+redled= 23
 GPIO.setup(redled,GPIO.OUT,initial=GPIO.LOW)
-GPIO.output(redled,GPIO.HIGH
+GPIO.output(redled,GPIO.HIGH)
 time.sleep(5)
